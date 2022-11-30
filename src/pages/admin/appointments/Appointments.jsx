dd804f44c7edb8381cc3d892eea8a29877147a26
Awais-06 <awaiskhalid0340@gmaile.com>
2022-11-30 19:27:15 +0500

    30 nov 4rth

--- src/pages/admin/appointments/Appointments.jsx
+++ src/pages/admin/appointments/Appointments.jsx
@@ -1,28 +1,46 @@
 import Paper from "@mui/material/Paper";
-import { ViewState } from "@devexpress/dx-react-scheduler";
-// import {
-//   Scheduler,
-//   DayView,
-//   Appointments,
-// } from "@devexpress/dx-react-scheduler-material-ui";
+import { AllDayPanel, ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  DateNavigator,
+  DayView,
+  Appointments,
+  ViewSwitcher,
+  MonthView,
+  Toolbar,
+  WeekView,
+  AppointmentTooltip,
+  TodayButton,
+  CurrentTimeIndicator,
+} from "@devexpress/dx-react-scheduler-material-ui";
 import React, { useState } from "react";
-import { dummy2 } from "../../../assets";
+import { clock, dummy2 } from "../../../assets";
 import { AppointmentView, SearchBar } from "../../../components";
 import Button from "@mui/material/Button";
 
 import "./appointments.css";
 const Appointment = () => {
-  const currentDate = "2018-11-01";
+  const currentDate = "2022-11-30T09:45";
   const schedulerData = [
     {
-      startDate: "2018-11-01T09:45",
-      endDate: "2018-11-01T11:00",
-      title: "Meeting",
+      startDate: "2022-11-30T09:45",
+      endDate: "2022-11-30T11:20",
+      title: "Appointment date",
     },
     {
-      startDate: "2018-11-01T12:00",
-      endDate: "2018-11-01T13:30",
-      title: "Go to a gym",
+      startDate: "2022-11-30T09:45",
+      endDate: "2022-11-30T11:20",
+      title: "Appointment date",
+    },
+    {
+      startDate: "2022-11-30T09:30",
+      endDate: "2022-11-30T11:00",
+      title: "Appointment date",
+    },
+    {
+      startDate: "2022-11-30T12:00",
+      endDate: "2022-11-30T13:30",
+      title: "Appointment date",
     },
   ];
   const [selectedType, setSelectedType] = useState({
@@ -106,6 +124,65 @@ const Appointment = () => {
     },
   ];
 
+  const AppointmentContent = ({ ...restProps }) => {
+    return (
+      <div {...restProps}>
+        {/* <div style={{ width: "2rem", height: "3rem", background: "red" }}>
+          <div className="kwn-side_divider" />
+        </div> */}
+      </div>
+    );
+  };
+  const TooltipContent = ({
+    appointmentData,
+    formatDate,
+    appointmentResources,
+    ...restProps
+    // #FOLD_BLOCK
+  }) => {
+    return (
+      <div
+        className="kwn-upcoming-appointment-calender-tool_tip-main_container"
+        {...restProps}
+      >
+        <div className="kwn-side_divider-calender" />
+        <div className="kwn-appointment-calender-tool_tip-container">
+          <div className="kwn-appointment-up_coming-tool_tip-title">
+            <h1>{appointmentData.title}</h1>
+          </div>
+          <div className="kwn-appointment-calender-tool_tip-date-main_container">
+            <img src={clock} />
+            <div className="kwn-appointment-calender-tool_tip-date-container">
+              <h3>
+                {formatDate(appointmentData.startDate, {
+                  day: "numeric",
+                  weekday: "long",
+                  month: "long",
+                  year: "numeric",
+                })}
+                <span>
+                  |
+                  {`${formatDate(appointmentData.startDate, {
+                    hour: "numeric",
+                    minute: "numeric",
+                  })}`}
+                </span>
+              </h3>
+            </div>
+          </div>
+          <hr />
+          <div style={{ marginTop: "2rem" }}></div>
+          <div className="kwn-appointment_profile_view">
+            <img src={dummy2} />
+            <div className="kwn-appointment_name_view">
+              <h3>vandarani aduhai</h3>
+              <h4>Depression</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
   return (
     <div className="kavan_admin_main_container">
       <div className="kwn-search">
@@ -135,13 +212,50 @@ const Appointment = () => {
           );
         })}
       </div>
-      <div className="kwn-appointsments-appointment_view">
-        {appointmentArray
-          .filter((item) => item.type === selectedType.title)
-          .map((item) => {
-            return <AppointmentView type={item.type} />;
-          })}
-      </div>
+      {
+        <div className="kwn-appointsments-appointment_view">
+          {appointmentArray
+            .filter((item) => item.type === selectedType.title)
+            .map((item) => {
+              return item.type === "Upcoming" ? (
+                <Paper>
+                  <Scheduler data={schedulerData}>
+                    <ViewState
+                      // currentDate={currentDate}
+                      defaultCurrentDate="2022-11-30T09:45"
+                    />
+                    <DayView
+                      displayName="Days"
+                      cellDuration={60}
+                      startDayHour={8}
+                      endDayHour={14}
+                    />
+                    <WeekView
+                      cellDuration={60}
+                      startDayHour={8}
+                      endDayHour={14}
+                    />
+
+                    <Toolbar />
+                    {/* <MonthView /> */}
+                    <Appointments containerComponent={AppointmentContent} />
+                    <TodayButton />
+                    <ViewSwitcher />
+
+                    <DateNavigator />
+                    <CurrentTimeIndicator />
+                    <AppointmentTooltip
+                      contentComponent={TooltipContent}
+                      // showCloseButton
+                    />
+                  </Scheduler>
+                </Paper>
+              ) : (
+                <AppointmentView type={item.type} />
+              );
+            })}
+        </div>
+      }
     </div>
   );
 };
--- src/pages/viewers/homePage/comp/Testimonial.jsx
+++ src/pages/viewers/homePage/comp/Testimonial.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { dummy2 } from "../../../../assets";
 import "./testimonial.css";
-import { Rate } from "antd";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
 
 const Testimonial = ({ item }) => {
+  const [value, setValue] = React.useState(2);
   return (
     <div className="kwn-home-testi-comp-main_container">
       <div className="kwn-home-testi-comp-text-container">
@@ -11,16 +14,18 @@ const Testimonial = ({ item }) => {
       </div>
       <div className="kvn-home-testi-com-review-main_container">
         <div className="kvn-home-testi-con-review-pro-pic-container">
-          <img alt='' src={dummy2} />
+          <img alt="" src={dummy2} />
         </div>
         <div className="kvn-home-testi-com-review-name-container">
           <h2>Rifki Abdurachman</h2>
-          <Rate
-            style={{ color: "#F8B84E" }}
-            allowHalf
-            defaultValue={2.5}
-            // onChange={setValue}
-            disabled
+          <Rating
+            name="simple-controlled"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            readOnly={true}
+            size="large"
           />
         </div>
       </div>
